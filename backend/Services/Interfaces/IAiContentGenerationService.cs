7853193d2cdb07ddcf1fd4975548541f8fb16a42
yuan07-0426 <2497739498@qq.com>
2026-09-07 15:45:15 +0800

    Add files via upload

--- backend/Services/Interfaces/IAiContentGenerationService.cs
+++ backend/Services/Interfaces/IAiContentGenerationService.cs
@@ -1,8 +1,11 @@
-﻿using PartySchoolApi.Models.DTOs;
+using PartySchoolApi.Models.DTOs;
 
 namespace PartySchoolApi.Services.Interfaces;
 
 public interface IAiContentGenerationService
 {
     Task<AiGenerateContentResponse> GenerateAsync(AiGenerateContentRequest request);
+
+    /// <summary>结合指定组织的党员数据生成党建宣讲稿</summary>
+    Task<SpeechGenerateResponse> GenerateSpeechWithDataAsync(SpeechGenerateRequest request);
 }
